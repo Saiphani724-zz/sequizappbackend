f50a114c99e3df59d406b4708fa521c6c45569e2
saiphani724 <saiphani724@gmail.com>
2020-03-15 23:11:16 +0530

    updated port configs

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 var bodyParser = require('body-parser');
 const app = express();
 var cors = require('cors');
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const MongoClient = require('mongodb').MongoClient;
